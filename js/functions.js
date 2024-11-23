f30b949c2c3d3ecd2e37a468541660abee57f636
Daria <ruksova17@gmail.com>
2024-11-23 22:13:47 +0400

    Удаляет тесты из functions.js

--- js/functions.js
+++ js/functions.js
@@ -26,27 +26,3 @@ const extractNumber = (string) => {
   }
   return parseInt(number);
 }
-
-console.log('\n--- Тесты для extractNumber---');
-console.log(
-  `Аргумент начинается с числа: ${extractNumber('2023 год') === 2023}`
-);
-console.log(
-  `Аргумент заканчивается на число: ${
-    extractNumber('ECMAScript 2022') === 2022
-  }`
-);
-console.log(
-  `Аргумент содержит несколько чисел, в том числе и с плавющей точкой: ${
-    extractNumber('1 кефир, 0.5 батона') === 105
-  }`
-);
-console.log(
-  `Аргумент закначивается на число с нолями: ${
-    extractNumber('агент 007') === 7
-  }`
-);
-console.log(`Аргумент строка без чисел: ${isNaN(extractNumber('а я томат'))}`);
-console.log(`Аргумент число: ${extractNumber(2023) === 2023}`);
-console.log(`Аргумент отрицательное число: ${extractNumber(-1) === 1}`);
-console.log(`Аргумент число с плавющей точкой: ${extractNumber(1.5) === 15}`);
