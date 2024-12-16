e869bc604703d32c1b8a52237f91ff6e8c095633
Daria <ruksova17@gmail.com>
2024-12-16 13:36:05 +0400

    Исправляет ошибку в импорте

--- js/create-photo.js
+++ js/create-photo.js
@@ -1,37 +1,37 @@
 import { getPhotoData } from './data.js';
 import { getRandomUniqueNumbers, getRandomArrayElement, getRandomNumber } from './utils.js';
 
+const PhotosQuantity = {
+  MIN: 1,
+  MAX: 25,
+};
+
+const AvatarsQuantity = {
+  MIN: 1,
+  MAX: 6,
+};
+
+const LikesQuantity = {
+  MIN: 15,
+  MAX: 200,
+};
+
+const CommentsIdQuantity = {
+  MIN: 1,
+  MAX: 1000,
+};
+
+const CommentsForPhotoQuantity = {
+  MIN: 0,
+  MAX: 30,
+};
+
+const MessagesForCommentQuantity = {
+  MIN: 1,
+  MAX: 2,
+};
+
 const createPhoto = () => {
-
-  const PhotosQuantity = {
-    MIN: 1,
-    MAX: 25,
-  };
-
-  const AvatarsQuantity = {
-    MIN: 1,
-    MAX: 6,
-  };
-
-  const LikesQuantity = {
-    MIN: 15,
-    MAX: 200,
-  };
-
-  const CommentsIdQuantity = {
-    MIN: 1,
-    MAX: 1000,
-  };
-
-  const CommentsForPhotoQuantity = {
-    MIN: 0,
-    MAX: 30,
-  };
-
-  const MessagesForCommentQuantity = {
-    MIN: 1,
-    MAX: 2,
-  };
 
   const generateCommentIds = getRandomUniqueNumbers(CommentsIdQuantity.MIN, CommentsIdQuantity.MAX);
   const generagePtohoIds = getRandomUniqueNumbers(PhotosQuantity.MIN, PhotosQuantity.MAX);
@@ -61,4 +61,4 @@ const createPhoto = () => {
   };
 };
 
-export { createPhoto };
+export { createPhoto, PhotosQuantity };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { createPhoto } from './create-photo.js';
+import { createPhoto, PhotosQuantity } from './create-photo.js';
 
-const photos = Array.from({length: createPhoto().PhotosQuantity.MAX}, createPhoto());
+const photos = Array.from({length: PhotosQuantity.MAX}, createPhoto);
 
 console.log(photos);
