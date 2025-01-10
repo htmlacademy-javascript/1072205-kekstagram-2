5a8452a019176022636737a9ddf02fbe68b2897f
Daria <ruksova17@gmail.com>
2025-01-10 14:47:13 +0400

    Добавляет загрузку комментариев и исправляет ошибки

--- js/full-size-photo-modal.js
+++ js/full-size-photo-modal.js
@@ -1,22 +1,24 @@
 import { createFullSizePhoto } from './create-full-size-photo.js';
+import { clearComments } from './show-сomments.js';
 
 const modalElement = document.querySelector('.big-picture');
 const closeElement = modalElement.querySelector('.big-picture__cancel');
 const thumbnailsContainerElement = document.querySelector('.pictures');
 const bodyElement = document.querySelector('body');
 
-const onEscapeDown = (evt) => {
+function onEscapeDown(evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
     closeModal();
-  };
-};
+  }
+}
 
-const closeModal = () => {
+function closeModal() {
   modalElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onEscapeDown);
-};
+  clearComments();
+}
 
 const openModal = (photos) => {
   thumbnailsContainerElement.addEventListener('click', (evt) => {
@@ -27,7 +29,7 @@ const openModal = (photos) => {
       modalElement.classList.remove('hidden');
       bodyElement.classList.add('modal-open');
       createFullSizePhoto(foundPhotoElement);
-    };
+    }
 
     document.addEventListener('keydown', onEscapeDown);
     closeElement.addEventListener('click', closeModal);
